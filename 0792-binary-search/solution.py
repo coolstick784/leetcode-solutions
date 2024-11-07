d26a389503eca7c84d54343eacbdde21d82c7c56
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2024-11-07 14:28:58 +0000

    [LeetCode Sync] Runtime - 1 ms (8.94%), Memory - 12.7 MB (100.00%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -1,14 +1,17 @@
 class Solution(object):
-    def search(self, nums, target, idx = 0):
-        mid = len(nums) // 2
-
-        
-        if target == nums[mid]:
-            return idx+mid
-        elif len(nums) == 1:
+    def search(self, nums, target, idx= 0):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        med = len(nums) // 2
+        if len(nums) == 1 and target != nums[0]:
             return -1
-        elif target > nums[mid]:
-            return self.search(nums[mid:], target, idx+mid)
+        if target > nums[med]:
+            return self.search(nums[med:], target, idx+med)
+        elif target < nums[med]:
+            return self.search(nums[:med], target, idx)
         else:
-            return self.search(nums[:mid], target, idx)
+            return idx + med
         
