9d574b56998712a75d043b876b5e7c24f2b5b33c
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-01-26 18:48:26 +0000

    [LeetCode Sync] Runtime - 9 ms (7.32%), Memory - 19.4 MB (10.14%)

--- 0036-valid-sudoku/solution.py
+++ 0036-valid-sudoku/solution.py
@@ -1,37 +1,32 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        def check_row(board):
-            for row in board:
-                non_blank = [el for el in row if el != '.']
-                if len(list(set(non_blank))) != len(non_blank):
-                    return False
-            return True
-        def check_col(board):
-            cur_col = []
-            for col in range(9):
-                cur_col = [row[col] for row in board]
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for r in board:
+            non_empty = [c for c in r if c != "."]
+            print(non_empty)
+            if len(non_empty) != len(list(set(non_empty))):
+                print("rows bad")
+                return False
+        cols = [[] for _ in range(9)]
+        for r in board:
+            for idx,c in enumerate(r):
+                cols[idx].append(c)
+        for col in cols:
+            non_empty = [c for c in col if c != "."]
+            if len(non_empty) != len(list(set(non_empty))):
+                print("cols bad")
+                return False
+        boxes = [[] for _ in range(9)]
+        #0,0 -> 0 0, 4 -> 1 0, 7 -> 2
+        # 3, 1 -> 3
 
-                non_blank = [el for el in cur_col if el != '.']
-                if len(list(set(non_blank))) != len(non_blank):
-                    return False
-            return True
-        def check_box(board):
-            for left in range(0, 9, 3):
-                for upper in range(0, 9, 3):
-                    cur_box = []
-                    for col in range(left, left+3):
-                        for row in range(upper, upper+3):
-                            cur_box.append(board[row][col])
-                    non_blank = [el for el in cur_box if el != '.']
-                    if len(list(set(non_blank))) != len(non_blank):
-                        return False
-            return True
-        if check_row(board) and check_col(board) and check_box(board):
-            return True
-        else:
-            return False
-            
+        for rnum, r in enumerate(board):
+            for cnum, c in enumerate(r):
+                multiplier = 3 * (rnum // 3)
+                adder = cnum // 3
+                boxes[multiplier + adder].append(c)
+        for b in boxes:
+            non_empty = [c for c in b if c != "."]
+            if len(non_empty) != len(list(set(non_empty))):
+                print("boxes bad")
+                return False
+        return True
