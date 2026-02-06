359a5a4dbcf28788fc63d462ed9be31d3fa1dcc6
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-02-06 02:03:39 +0000

    [LeetCode Sync] Runtime - 46 ms (57.44%), Memory - 19.3 MB (31.77%)

--- 0007-reverse-integer/solution.py
+++ 0007-reverse-integer/solution.py
@@ -1,41 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
         if x < 0:
-            start = "-"
+            neg = -1
         else:
-            start = ""
+            neg = 1
         x = abs(x)
-        as_l = list(str(x))
-        compare = 2**31
-        as_l.reverse()
-        as_str_no_sign =  "".join(as_l)
-        as_str = start + as_str_no_sign
-        def outOfRange(cur, compare):
-            if start == "-":
-                neg = True
-            else:
-                neg = False
-            
-            comp_str = list(str(compare))
-            if len(cur) > len(comp_str):
-                return True
-            if len(cur) < len(comp_str):
-                return False
-            for idx, ch in enumerate(cur):
-                cur_comp = int(comp_str[idx])
-                if idx == len(cur) - 1 and neg==False:
-                    cur_comp -= 1
-                
-                if int(ch) < cur_comp:
-                    return False
-                elif int(ch) == cur_comp and idx == len(cur) - 1:
-                    return False
-                elif int(ch) > cur_comp:
-                    return True
-            return True
+        rev_x = str(x)[::-1]
 
-        if outOfRange(as_str_no_sign, compare):
+        int_rev_x = int(rev_x) * neg
+        if int_rev_x > 2**31-1 or int_rev_x < -2**31:
             return 0
         else:
-            return int(as_str)
+            return int_rev_x
+
         
