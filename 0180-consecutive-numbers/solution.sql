ff3ad29e93ff7c99b49681611657765e2c1ec5d4
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-02-20 02:16:57 +0000

    [LeetCode Sync] Runtime - 265 ms (65.37%), Memory - 0.0B (100.00%)

--- 0180-consecutive-numbers/solution.sql
+++ 0180-consecutive-numbers/solution.sql
@@ -1,6 +1,7 @@
 -- Write your PostgreSQL query statement below
-select distinct num as ConsecutiveNums from (
-select id, num, lag(num, 1) over () as lag1, lag(num, 2) over () as lag2  from Logs
+with lag as (
+select distinct num, lag(num, 1) over () as lag1, lag(num, 2) over () as lag2
+from Logs
 )
-where num = lag1 and num = lag2
-
+select distinct num as ConsecutiveNums from lag 
+where num = lag1 and num=lag2
