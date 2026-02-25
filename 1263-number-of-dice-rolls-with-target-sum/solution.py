51f7745e90e495b94e259e34d52c75adf5224468
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-02-25 21:38:16 +0000

    [LeetCode Sync] Runtime - 171 ms (78.32%), Memory - 19.6 MB (69.91%)

--- 1263-number-of-dice-rolls-with-target-sum/solution.py
+++ 1263-number-of-dice-rolls-with-target-sum/solution.py
@@ -1,32 +1,26 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        # For each number n 1 to k, add the number of combinations for each other die summing to target - n
+        # 1. Get the number of possibilites that are still possible to reach the target, with their counts for die j - 1
+        # Add 1 to each possibily for each i from 1 to k 
         
-        # Only keep ones that are possible, so at each n, consider if it can be eventually equal to the target
-
-        res = 0
-        ctr = {}
-        new_ctr = {}
+        cur_poss = {0:1}
+        next_poss = {}
         for die in range(1, n+1):
 
-            for roll in range(1, k+1):
-                if ctr == {}:
-                    new_ctr[roll] = 1
-                else:
-                    for c in ctr:
-                        cur = roll + c
-
-                            
-
-
-                        if cur <= target - (n - die) and cur >= target -( n - die) * k: 
-                            new_ctr[cur] = new_ctr.get(cur, 0) + ctr[c]
-
-            ctr = new_ctr.copy()
-            new_ctr = {}
-        if ctr.get(target, 0) == 0:
+            next_poss = {}
+            #possible_min = target - 6*(n - die)
+            #possible_max = target - n - die
+            for num in cur_poss:
+                ct = cur_poss[num]
+                for j in range(1, k+1):
+                    next_poss[num+j] = next_poss.get(num+j, 0) + ct
+                    
+                    
+            
+            
+            cur_poss = next_poss.copy()
+        if cur_poss.get(target, 0) == 0:
             return 0
-        return ctr[target] % (10**9 + 7)
-                
-
-
+        return cur_poss[target] % (10**9+7)
+        
+        
