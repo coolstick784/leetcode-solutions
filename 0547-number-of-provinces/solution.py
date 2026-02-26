860a82c02d3d4bb45587097d92351b328bcae91d
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-02-26 00:22:57 +0000

    [LeetCode Sync] Runtime - 7 ms (47.91%), Memory - 20.6 MB (48.34%)

--- 0547-number-of-provinces/solution.py
+++ 0547-number-of-provinces/solution.py
@@ -1,34 +1,29 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # Look at each connection, and then set the property blank after looking through it
-        # Look at 1 -> 2 and set the connection from 1 to 2 to null both ways, then look at 2-> anything else, and so on
-        # Then look at the connections from 2, and keep going until we reach the end
-
         res = 0
-        no_conns = [0 for _ in range(len(isConnected[0]))]
-        conns = set()
-
-        def connectProvinces(r):
-            cur_row = isConnected[r]
-            for idx, el in enumerate(cur_row):
-                if el == 1:
-                    conns.add(idx)
-                    isConnected[r][idx] = 0
-                    isConnected[idx][r] = 0
-                    connectProvinces(idx)
-
-
-
-
+        conn_dict = {}
         for r in range(len(isConnected)):
-            isConnected[r][r] = 0
-        for r in range(len(isConnected)):
-            if r not in conns:
+            for c in range(len(isConnected[0])):
+
+                if isConnected[r][c] == 1:
+                    conn_dict.setdefault(r, []).append(c)
+        print("conn dict", conn_dict)
+                    
+        
+        q = deque([])
+        for r in conn_dict:
+            if conn_dict[r] != []:
+        
+                q.append(r)
+                while q:
+     
+                    cur = q.popleft()
+                    for c in conn_dict[cur]:
+                        q.append(c)
+                    conn_dict[cur] = []
                 res += 1
-                conns.add(r)
-                connectProvinces(r)
-
-
+                
+                    
+                        
         return res
-            
-
+                    
