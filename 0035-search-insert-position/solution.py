843f7f847eb68ecd0152d69d2bd60a8367c401ff
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-02-26 00:25:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20 MB (44.06%)

--- 0035-search-insert-position/solution.py
+++ 0035-search-insert-position/solution.py
@@ -1,36 +1,26 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-       cur_idx = int(len(nums) / 2)
-       forward_backward = "none"
-       while True:
-            if nums == [] and forward_backward != "forward":
-                return cur_idx
-            elif nums == []:
-                return cur_idx + 1
-            mid_idx = int(len(nums) / 2)
-            if forward_backward == "forward":
-                cur_idx += mid_idx + 1
-            elif forward_backward == "backward":
-                cur_idx -= (len(nums) - mid_idx)
-            print(nums)
-            print("cur_idx", cur_idx)
-            print("mid_idx", mid_idx)
-
-            print("current middle", nums[mid_idx])
-
-            if len(nums) == 1:
-                if target <= nums[0]:
-                    return cur_idx
-                elif target == nums[0]:
-                    return cur_idx
-                else:
-                    return cur_idx + 1
+        
+        left = 0 
+        right = len(nums) - 1
+        if target < nums[left]:
+            return 0
+        if target > nums[right]:
+            return len(nums)
+        while left < right - 1:
             
-            if target > nums[mid_idx]:
-                nums = nums[mid_idx + 1:]
-                forward_backward = "forward"
-            elif target == nums[mid_idx]:
-                return cur_idx
+
+            med = (right + left) // 2
+     
+          
+            if nums[med] == target:
+                return med
+            elif nums[med] > target:
+
+                right = med
             else:
-                nums = nums[:mid_idx]
-                forward_backward = "backward"
+
+                left = med
+        if nums[left] == target:
+            return left
+        return right
