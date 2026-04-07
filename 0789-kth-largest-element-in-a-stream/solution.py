a0f2f127ff19e73776039645a98658478471a275
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-04-07 20:31:24 +0000

    [LeetCode Sync] Runtime - 44 ms (11.75%), Memory - 25.4 MB (59.32%)

--- 0789-kth-largest-element-in-a-stream/solution.py
+++ 0789-kth-largest-element-in-a-stream/solution.py
@@ -1,48 +1,15 @@
-class KthLargest(object):
+class KthLargest:
 
-    def __init__(self, k, nums):
-        """
-        :type k: int
-        :type nums: List[int]
-        """
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.nums.sort()
         self.k = k
-        self.nums = sorted(nums)
-        
         
 
-    def add(self, val):
-        """
-        :type val: int
-        :rtype: int
-        """
-
-        self.nums.insert(self.insertVal(val, self.nums), val)
-        #print("new", self.nums)
+    def add(self, val: int) -> int:
+        insert_pos = bisect.bisect(self.nums, val)
+        self.nums.insert(insert_pos, val)
         return self.nums[-self.k]
-    def insertVal(self,val, nums, idx = 0):
-        #print("val", val)
-        #print("nums", nums)
-        
-        med_idx = len(nums) // 2
-        #print("med idx", med_idx)
-        if nums == [] or val <= nums[0]:
-            #print("return1")
-            return idx
-        if val >= nums[-1]:
-            #print("return2")
-            return idx + len(nums)
-        if val >= nums[0] and val <= nums[1]:
-            #print("return3")
-            #print(idx+1)
-            return idx+1
-        if val == nums[med_idx]:
-            #print("return")
-            return idx + med_idx
-        if val > nums[med_idx]:
-            return self.insertVal(val, nums[med_idx:], idx+med_idx)
-        elif val < nums[med_idx]:
-            return self.insertVal(val, nums[:med_idx], idx)
-
 
 
 # Your KthLargest object will be instantiated and called as such:
