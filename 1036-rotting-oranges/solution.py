9473433acbb68d59407d923a08b3b2c8ae46cbd8
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-04-07 21:19:36 +0000

    [LeetCode Sync] Runtime - 4 ms (36.45%), Memory - 19.3 MB (41.53%)

--- 1036-rotting-oranges/solution.py
+++ 1036-rotting-oranges/solution.py
@@ -1,47 +1,51 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        num_rows = len(grid)
-        num_cols = len(grid[0])
-        rotten = {} # (2, 1): 2
-        fresh = {} # (2, 1): 1
-        for r in range(num_rows):
-            for c in range(num_cols):
-                cur_el = grid[r][c]
-                if cur_el == 1:
-                    fresh[(r, c)] = 1
-                elif cur_el == 2:
-                    rotten[(r, c)] = 2
-        res = 0
-        while rotten:
-            new = {}
-
-
-
-            for r, c in rotten:
-                
-                if fresh.get((r-1, c), 0) == 1:
-                    new[(r-1, c)] = 2
-                    fresh[(r-1, c)] = 0
-
-                if fresh.get((r+1, c), 0) == 1:
-                    new[(r+1, c)] = 2
-                    fresh[(r+1, c)] = 0
-                if fresh.get((r, c+1), 0) == 1:
-
-                    new[(r, c+1)] = 2
-       
-                    fresh[(r, c+1)] = 0
-                if fresh.get((r, c-1), 0) == 1:
+        # start with each rotten orange
+        # then, for each rotten orange, we want a new list of rotten oranges that were not rotten previously
+        # for those oranges, they are set with minute 1
+        # then for those oranges, that's minute two, and we keep going until there are no more fresh oranges to rot
+        
+        rotten = set()
+        rot_from = set()
+        new_rot_from = set()
+        fresh = set()
+        
+        for r, row in enumerate(grid):
+            for c, el in enumerate(row):
+                if el == 2:
+                    rotten.add((r, c))
+                if el == 1:
+                    fresh.add((r, c))
                     
-                    new[(r, c-1)] = 2
-                    fresh[(r, c-1)] = 0
-            res += 1
-
-            rotten = new.copy()
-        res -= 1
-        res = max(0, res)
-        if fresh and max(fresh.values()) == 1:
-            return -1
+        if not rotten and not fresh:
+            return 0
+     
+                    
+        def rot(row, col):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or (row, col) in rotten or grid[row][col] != 1:
+                return
+            grid[row][col] == 2
+            new_rot_from.add((row, col))
+            
+            
+        rot_from = rotten.copy()
         
-        return res
-        
+        minutes = -1
+        while rot_from:
+      
+            for r, c in rot_from:
+                rot(r-1, c)
+                rot(r+1, c)
+                rot(r, c-1)
+                rot(r, c+1)
+            rotten = rotten.union(new_rot_from)
+            rot_from = new_rot_from.copy()
+            new_rot_from = set()
+           
+            minutes += 1
+       
+        for r, row in enumerate(grid):
+            for c, el in enumerate(row):
+                if el == 1 and (r, c) not in rotten:
+                    return -1
+        return minutes
