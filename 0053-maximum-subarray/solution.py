121368a721aa1f615cec38c8077d5843134ee7b2
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-04-08 16:07:08 +0000

    [LeetCode Sync] Runtime - 28 ms (79.11%), Memory - 31.6 MB (6.39%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,15 +1,19 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        total = 0
-        cur_max = min(nums)
+# 1. if a running sum if ever <0, remove everything up to the next index and restart the running sum
+# if all numbers are <0, return the maximum of nums
+# if a running sum is > 0, then the resolution is the max of (res, running_sum)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = 0
+        run_sum = 0
+        max_val = max(nums)
+        if max_val < 0:
+            return max_val
         for idx, n in enumerate(nums):
-            total += n
-            if total > cur_max:
-                cur_max = total
-            total = max(0, total)
-        return cur_max
-            
+            run_sum += n
+            if run_sum < 0:
+                run_sum = 0
+                
+            else:
+                res = max(res, run_sum)
+
+        return res
