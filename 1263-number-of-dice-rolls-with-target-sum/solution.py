ba7af8d55dd2c502a4f6a50ea50f4762570370cb
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-04-16 19:28:56 +0000

    [LeetCode Sync] Runtime - 223 ms (64.66%), Memory - 19.3 MB (95.42%)

--- 1263-number-of-dice-rolls-with-target-sum/solution.py
+++ 1263-number-of-dice-rolls-with-target-sum/solution.py
@@ -1,26 +1,20 @@
+# use dp here
+# so basically, we want the counts of everything up to it, and the new counts
+# so originally we start with {0:1}
+# then, for each number to k, add to the new dp with the new count
+# if it's > target, don't include it
+# so .eg. if our face is 5 and we start at 3, dp[5] = d[3] from 2 and add them and so on
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        # 1. Get the number of possibilites that are still possible to reach the target, with their counts for die j - 1
-        # Add 1 to each possibily for each i from 1 to k 
+        cur_dp = {0:1}
+        new_dp = {}
         
-        cur_poss = {0:1}
-        next_poss = {}
-        for die in range(1, n+1):
-
-            next_poss = {}
-            #possible_min = target - 6*(n - die)
-            #possible_max = target - n - die
-            for num in cur_poss:
-                ct = cur_poss[num]
-                for j in range(1, k+1):
-                    next_poss[num+j] = next_poss.get(num+j, 0) + ct
-                    
-                    
-            
-            
-            cur_poss = next_poss.copy()
-        if cur_poss.get(target, 0) == 0:
-            return 0
-        return cur_poss[target] % (10**9+7)
-        
-        
+        for die in range(n):
+            for face in range(1, k+1):
+                for prev in cur_dp:
+                    if prev+face > target:
+                        continue
+                    new_dp[prev+face] = (new_dp.get(prev+face, 0) + cur_dp[prev]) % (10**9+7)
+            cur_dp = new_dp.copy()
+            new_dp = {}
+        return cur_dp.get(target, 0) % (10**9+7)
