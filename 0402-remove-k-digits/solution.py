2a31286b678aa1774e6290d56436153feb7ad48c
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-04-16 18:00:18 +0000

    [LeetCode Sync] Runtime - 19 ms (77.86%), Memory - 22.9 MB (5.01%)

--- 0402-remove-k-digits/solution.py
+++ 0402-remove-k-digits/solution.py
@@ -1,18 +1,21 @@
+# starting from the left, if a number is greater than the number directly to the right of it, remove it
+# continue doing this until we've either removed k elements or it's in non-descending order
+#then, remove the rightmost number until we've reached k deletions
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
+        if k == len(num):
+            return "0"
         stack = []
-
-        for digit in num:
-            while k > 0 and stack and stack[-1] > digit:
+        deletions = 0
+        for idx, n in enumerate(num):
+            as_i = int(n)
+            while stack and as_i < stack[-1] and deletions < k:
                 stack.pop()
-                k -= 1
-
-            stack.append(digit)
-
-        while k > 0:
-            stack.pop()
-            k -= 1
-
-        result = ''.join(stack).lstrip('0')
-
-        return result if result else "0"
+                deletions += 1
+            stack.append(as_i)
+        final_len = len(num) - k
+        stack = stack[:final_len]
+        res =  "".join([str(s) for s in stack]).lstrip("0")
+        if res == "":
+            return "0"
+        return res
