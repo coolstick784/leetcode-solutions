b7baf5a07672f9c20ab669c8e1068d939063675a
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-05-24 03:01:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.8 MB (80.43%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,22 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ctr = {}
-        for n in nums:
-            ctr[n] = ctr.get(n, 0) + 1
-        print(ctr)
-        at_each = {}
-        for c in ctr:
-
-            at_each.setdefault(ctr[c], []).append(c)
-
-
-        res = []
-        start = len(nums)
-        out_len = 0
-        while out_len != k:
-            
-            res.extend(at_each.get(start, []))
-            if at_each.get(start, []) != []:
-                out_len += len(at_each[start])
-            start -= 1
-        return res
+        ctr = Counter(nums)
+        heap = []
+        for n, ct in ctr.items():   
+            heapq.heappush(heap, (ct, n))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [n for ct, n in heap]
