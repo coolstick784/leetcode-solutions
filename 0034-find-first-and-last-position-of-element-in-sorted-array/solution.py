e5cafa102f10acb074c189ea48edeebb419c952f
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-05-24 01:14:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (28.71%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,54 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = 0
-        right = len(nums) - 1
-        if len(nums) == 1 and nums[0] == target:
-            return [0,0]
-        def findFirstLeft(nums, target, cur_right, cur_left):
+ 
 
-            
-            while cur_left <= cur_right:
-                print("finding left", cur_left, cur_right)
-                cur_med = (cur_left + cur_right) // 2
-                if nums[cur_med] == target and (cur_med == 0 or nums[cur_med-1] < target):
-                    return cur_med
-                elif nums[cur_med] > target:
-                    cur_right = cur_med - 1
-                elif nums[cur_med] < target :
-                    cur_left = cur_med + 1
+        def findLowest():
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                med = (left + right) // 2
+ 
+                if target > nums[med]:
+                    left = med + 1
+                elif nums[med] == target and (med == 0 or nums[med-1] < target):
+                    return med
                 else:
-                    cur_right = cur_med - 1
-            return cur_right
-        def findLastRight(nums, target, cur_left, cur_right):
+                    right = med - 1
+            return -1
 
-            
-            while cur_left <= cur_right:
-                print("finding right")
-                cur_med = (cur_left + cur_right) // 2
-                if nums[cur_med] == target and (cur_med == len(nums) - 1 or nums[cur_med+1] > target):
-                    return cur_med
-                elif nums[cur_med] > target:
-                    cur_right = cur_med - 1
-                elif nums[cur_med] < target:
-                    cur_left = cur_med + 1
+        def findHighest():
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                med = (left + right) // 2
+ 
+                if nums[med] > target:
+                    right = med - 1
+                elif nums[med] == target and (med == len(nums)-1 or nums[med+1] > target):
+                    return med
                 else:
-                    cur_left = cur_med + 1
-            return cur_right
-                
+                    left = med + 1
+            return -1
 
-        while(left <= right):
-            med = (left + right) // 2
-            if nums[med] < target:
-                left = med + 1
-            elif nums[med] > target:
-                right = med - 1
-            elif nums[left] == target and nums[right] == target and (left == 0 or nums[left-1] < target) and (right == len(nums)-1 or nums[right+1] > target):
-                return [left, right]
-            else:
-                print("finding")
-                left = findFirstLeft(nums, target, med, left)
-                right = findLastRight(nums, target, med, right)
-                
-                return [left, right]
-
-        return [-1, -1]
+        return [findLowest(), findHighest()]
