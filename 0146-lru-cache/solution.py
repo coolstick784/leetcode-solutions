4d7d322892316a6c8f1108e48c3b3f91c8b89dd8
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-05-27 17:56:20 +0000

    [LeetCode Sync] Runtime - 122 ms (48.01%), Memory - 76.7 MB (75.82%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,38 +1,21 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dict = {}
-        self.q = deque()
-        self.ctr_keys = {}
-        self.total = 0
+        self.od = OrderedDict()
         self.capacity = capacity
-        
 
     def get(self, key: int) -> int:
-        if key in self.dict:
-            self.q.append(key)
-            self.ctr_keys[key] = self.ctr_keys.get(key, 0) + 1
-            return self.dict.get(key, -1)
-        else:
-            return -1
-        
 
+        if key in self.od:
+            self.od.move_to_end(key)
+        return self.od.get(key, -1)
     def put(self, key: int, value: int) -> None:
-        self.q.append(key)
+        if key in self.od:
+            self.od.move_to_end(key)
 
-        self.ctr_keys[key] = self.ctr_keys.get(key, 0) + 1
-        if key not in self.dict:
-            self.total += 1
-            self.dict[key] = value
-            while self.total > self.capacity:
-                to_remove = self.q.popleft()
-                self.ctr_keys[to_remove] -= 1
-                if self.ctr_keys[to_remove] == 0:
-                    del self.dict[to_remove]
-                    self.total -= 1
-        else:
-            self.dict[key] = value
-
+        elif len(self.od) == self.capacity:
+            self.od.popitem(last=False)
+        self.od[key] = value
 
 
 # Your LRUCache object will be instantiated and called as such:
