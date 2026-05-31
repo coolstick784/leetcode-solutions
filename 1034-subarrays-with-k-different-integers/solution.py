0ef367a180758489ff0831217b432f681fa71931
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-05-31 18:37:00 +0000

    [LeetCode Sync] Runtime - 107 ms (90.59%), Memory - 22.2 MB (22.66%)

--- 1034-subarrays-with-k-different-integers/solution.py
+++ 1034-subarrays-with-k-different-integers/solution.py
@@ -1,118 +1,34 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        left = 0
+        left= 0
+        lr = 0
         right = 0
+        s = set()
         ctr = {}
-        uniq = 0
-
- 
-        din = "right"
-        
-        # if there are too many, move the left over 
-        # if there are the exact amount or too little, move the right over
-
-        # we want to know that there are n distinct integers, and that if we extend mul units over, there will still be n distinct integers. that is our mul
-        # find the smallest subarrays, and then the largest subarrays, and add their differences to the total
-        # the smallest and largest subarrays should end at the same number
-        ends = {}
-        past_equal = False
-
-        while left < len(nums) and right < len(nums):
-            if din == "right":
-                cur = nums[right]
-                if ctr.get(cur, 0) == 0:
-                    ctr[cur] = 1
-                    uniq += 1
-                    #print("cur", cur)
-                    #print("uniq", uniq)
-                else:
-                    ctr[cur] += 1
-      
-            else:
-                cur = nums[left-1]
-                if ctr[cur] == 1:
-                    uniq -= 1
-                    ctr[cur] = 0
-                    
-                   
-                else:
-                    ctr[cur] -= 1
-
-
-            
-            if uniq <= k:
-                if uniq == k:
-                    ends[right] = left
-                din = "right"
-                right += 1
-            else:
-
-
-                din = "left"
-                left += 1
-
-
-        left = 0
-  
-        right = 0
-  
-
-        ctr = {}
-        uniq = 0
         res = 0
-
-        rights = set(ends.keys())
-        past_equal= False
-        print("ends", ends)
-        
-
-        while left < len(nums) and right < len(nums):
-            if din == "right":
-                cur = nums[right]
-                if ctr.get(cur, 0) == 0:
-                    ctr[cur] = 1
-                    uniq += 1
-                    #print("cur", cur)
-                    #print("uniq", uniq)
-                else:
-                    ctr[cur] += 1
-      
-            else:
-                cur = nums[left-1]
-                if ctr[cur] == 1:
-                    uniq -= 1
-                    ctr[cur] = 0
-                    
-                   
-                else:
-                    ctr[cur] -= 1
-
-
+        while right < len(nums):
+            n = nums[right]
+            s.add(n)
+            ctr[n] = ctr.get(n, 0) + 1
             
-            if uniq < k:
-                if past_equal:
-                    print("left", left-1)
-                    print("ends", ends[right])
 
-                    res += (left-1) - ends[right] + 1
-                din = "right"
-                right += 1
-            else:
+            if len(s) > k:
+                left = lr
+                s.remove(nums[lr-1])
+            if len(s) == k:
+
+                while len(ctr.keys()) == k:
+                    l = nums[lr]
+              
+                    ctr[l] -= 1
+                    lr += 1
+                    if ctr[l] == 0:
+                        del ctr[l]
+                res += (lr-left)
 
 
-                din = "left"
-                left += 1
-                if uniq == k:
 
-                    past_equal = True
-                else:
-                    past_equal = False
-        if past_equal and left == len(nums):
-            res += left - ends[len(nums) - 1] 
-        
+
+            right += 1
 
         return res
-
-
-
-
