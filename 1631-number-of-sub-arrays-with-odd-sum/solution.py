8789ab96eaf83f655ef3a433413dda615f4edf83
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-06-09 14:46:13 +0000

    [LeetCode Sync] Runtime - 91 ms (40.88%), Memory - 24.6 MB (29.01%)

--- 1631-number-of-sub-arrays-with-odd-sum/solution.py
+++ 1631-number-of-sub-arrays-with-odd-sum/solution.py
@@ -1,31 +1,18 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        num_odd = [0 for _ in arr]
-        num_even = [0 for _ in arr]
-        new = []
-        for n in arr:
-            if n % 2:
-                new.append(False)
-            else:
-                new.append(True)
+        odd_even = [n%2 == 0 for n in arr]
+        num_even = 0
+        num_odd = 0
         res = 0
-        cur_odd = 0
-        cur_even = 0
-        for idx in range(len(arr)-1, -1, -1):
-            cur = new[idx]
-            if cur:
-                cur_even += 1
+        for n in odd_even:
+            if n:
+                num_even += 1
             else:
-                cur_odd += 1
-            if idx == len(arr) - 1:
-                num_odd[idx] = cur_odd
-                num_even[idx] = cur_even
-            elif cur:
-                num_odd[idx] = num_odd[idx+1]
-                num_even[idx] = 1 + num_even[idx+1]
-            else:
-                num_odd[idx] = 1 + num_even[idx+1]
-                num_even[idx] = num_odd[idx+1]
-            res += num_odd[idx]
+                num_even, num_odd = num_odd, num_even
+                num_odd += 1
+         
+            res += num_odd
             res = res % (10**9+7)
+                
+
         return res
