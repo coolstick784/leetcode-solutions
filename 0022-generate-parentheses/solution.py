3aa75458df1c8f7118ff0fb0c5540f932b59d82f
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-06-15 15:58:49 +0000

    [LeetCode Sync] Runtime - 2 ms (34.11%), Memory - 19.7 MB (9.38%)

--- 0022-generate-parentheses/solution.py
+++ 0022-generate-parentheses/solution.py
@@ -1,23 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        out = []
-        cur_s = ""
-        cur_stack = []
-        final_len = n*2
-        openings = ["(" for _ in range(n)]
-        closings = [")" for _ in range(n)]
-        def generateNext(cur, stack, cur_open, cur_close):
+        
+        def solve(left, stack):
+            if left < 0 or stack < 0 or (left <= 0 and stack <= 0):
+                return []
+            
+            if left == 0:
+                return [")" * stack]
+            if left == 1 and stack == 0:
+                return ["()"]
+            out = []
+            if stack:
+                for sol in solve(left, stack-1):
+                    out.append(")" + sol)
+            if left:
+                for sol in solve(left-1, stack+1):
+                    out.append("(" + sol)
+            
+            return list(set(out))
 
-            if stack != []:
-                print("adding close")
-                generateNext(cur + ")", stack[:-1], cur_open, cur_close[:-1])
-            if cur_open != []:
-
-      
-                generateNext(cur + "(", stack + ["("], cur_open[:-1], cur_close)
-            if cur_open == [] and cur_close == []:
-
-                out.append(cur)
-        generateNext(cur_s, cur_stack, openings, closings)
-        return out
-
+        
+        return solve(n, 0)
