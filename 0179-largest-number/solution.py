5c9e87affaa6d0a0310a93f62ddf8963139c7ad2
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-06-22 17:59:39 +0000

    [LeetCode Sync] Runtime - 3 ms (62.62%), Memory - 19.4 MB (25.63%)

--- 0179-largest-number/solution.py
+++ 0179-largest-number/solution.py
@@ -1,28 +1,13 @@
-from typing import List
 from functools import cmp_to_key
-
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # 1) convert to strings
-        store = [str(num) for num in nums]
+        nums = [str(n) for n in nums]
 
-        # 2) comparator: decide order of two strings a and b
-        def compare(a: str, b: str) -> int:
-            # if putting a before b makes a bigger combined number, a should come first
+        def compare(a, b):
             if a + b > b + a:
-                return -1   # a comes before b
-            elif a + b < b + a:
-                return 1    # b comes before a
+                return -1
             else:
-                return 0    # either order is fine
-
-        # 3) sort using the comparator
-        store.sort(key=cmp_to_key(compare))
-
-        # 4) handle all zeros
-        if store[0] == "0":
-            return "0"
-
-        # 5) join into the final answer
-        return "".join(store)
-
+                return 1
+        res =  "".join(sorted(nums, key=cmp_to_key(compare))).lstrip("0")
+        return "0" if not res else res
+        
