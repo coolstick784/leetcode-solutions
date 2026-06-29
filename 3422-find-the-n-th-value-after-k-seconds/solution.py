6577d1dd7c0cdcb1f1d603d12246bb8534eeca66
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-06-29 21:36:37 +0000

    [LeetCode Sync] Runtime - 507 ms (76.47%), Memory - 178.7 MB (9.63%)

--- 3422-find-the-n-th-value-after-k-seconds/solution.py
+++ 3422-find-the-n-th-value-after-k-seconds/solution.py
@@ -1,17 +1,19 @@
-# 4 + 3 + 2 + 1 , 1*4 + 1*3 + 1*2 + 1*1 = 10
-# 4 + 3 + 2 + 1 + 3 + 2 + 1 + 2 + 1 + 1, 1*4 + 2*3 + 3*2 + 4*1 = 20
-# 4 + 3 + 2 + 1 + 3 + 2 + 1 + 2 + 1 + 1 + 3 + 2 + 1 + 2 + 1 + 1 + 2 + 1 + 1 + 1, 1*4 + 3*3+6*2+1*10 = 35
+MOD = 10**9+7
+@lru_cache(None)
+def solve(curN, curK):
+    if curK == 0:
+        return 1
+    if curK == 1:
+        return curN
+    if curN == 1:
+        return 1
+    if curN == 2:
+        return curK + 1
+    return (solve(curN, curK-1) + solve(curN-1, curK)) % MOD
 
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        prev = [None for _ in range(n)]
-        cur = [1 for _ in range(n)]
-        for i in range(k):
-            prev = cur.copy()
-            cur_s = 0
-            for idx, p in enumerate(prev):
-                cur[idx] = p + cur_s
-                cur_s += p
 
-        
-        return cur[-1] % (10**9+7)
+
+
+        return solve(n, k) % MOD
