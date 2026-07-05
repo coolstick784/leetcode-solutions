3c416172f25da1babdc7b02a26a8ff2c95e217b9
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-07-05 23:22:54 +0000

    [LeetCode Sync] Runtime - 60 ms (84.27%), Memory - 70.7 MB (53.23%)

--- 3501-delete-nodes-from-linked-list-present-in-array/solution.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/solution.py
@@ -5,13 +5,19 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        ns = set(nums)
-        
-        def solve(node):
-            while node and node.val in ns:
-                node = node.next
-            if node:
-                node.next = solve(node.next)
-            return node
-        return solve(head)
+        nums = set(nums)
+        while head.val in nums:
+            head = head.next
+        cur_node = head.next 
+        prev = head
+        while cur_node:
+            new = cur_node.next
+            if cur_node.val in nums:
+                prev.next = new
+                cur_node = new
+            else:
+                prev = cur_node
+                cur_node = new
+
+        return head
+
