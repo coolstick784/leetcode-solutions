1ba682c4b930b62ad18233a4fbb96352bbd720df
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-08-30 12:45:42 +0000

    [LeetCode Sync] Runtime - 51 ms (9.58%), Memory - 33.5 MB (66.99%)

--- 2212-removing-minimum-and-maximum-from-array/solution.py
+++ 2212-removing-minimum-and-maximum-from-array/solution.py
@@ -1,18 +1,13 @@
-# we can either delete max and min from the left, max from the left and min from the right, max from the right and min from the left, or both from the right
-# if max idx < min idx obviously we would need to remove max from the left and min from the right and vice versa
-
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        min_val = min(nums)
-        max_val = max(nums)
-        min_idx = nums.index(min_val)
-        max_idx = nums.index(max_val)
-        from_left = max(min_idx, max_idx) + 1
-        from_right = max(len(nums)-min_idx, len(nums)-max_idx)
-        if min_idx < max_idx:
-            from_both = min_idx+1 + (len(nums) - max_idx)
-        else:
-            from_both = max_idx + 1 + (len(nums)-min_idx)
+        mx = max(nums)
+        mn = min(nums)
+        for idx, n in enumerate(nums):
 
-        return min([from_left,from_right,from_both])
+            if n == mx:
+                mx_idx = idx
+            if n == mn:
+                mn_idx = idx
         
+        mx_idx, mn_idx = max(mx_idx, mn_idx), min(mx_idx, mn_idx)
+        return min(mx_idx+1, len(nums) - mn_idx, mn_idx+1 + len(nums) - mx_idx)
