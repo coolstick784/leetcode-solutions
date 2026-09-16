509d1cb245596bb36cc84fdacadfa7cf61f74d37
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-09-16 19:21:51 +0000

    [LeetCode Sync] Runtime - 48 ms (53.36%), Memory - 29.5 MB (55.76%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,21 +1,30 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.od = OrderedDict()
+        self.dict = {} # key: (ctr, value)
+        self.heap = [] # (ctr, key)
+        self.ctr = 0
         self.capacity = capacity
 
     def get(self, key: int) -> int:
+        if not self.dict.get(key):
+            return -1
+        self.ctr += 1
+        heapq.heappush(self.heap, (self.ctr, key))
+        _, val = self.dict[key]
+        self.dict[key] = (self.ctr, val)
+        return self.dict[key][1]
+        
 
-        if key in self.od:
-            self.od.move_to_end(key)
-        return self.od.get(key, -1)
     def put(self, key: int, value: int) -> None:
-        if key in self.od:
-            self.od.move_to_end(key)
-
-        elif len(self.od) == self.capacity:
-            self.od.popitem(last=False)
-        self.od[key] = value
+        self.ctr += 1
+        self.dict[key] = (self.ctr, value)
+        while self.heap and self.dict[self.heap[0][1]][0] != self.heap[0][0]:
+            heapq.heappop(self.heap)
+        if len(self.dict.keys()) > self.capacity:
+            _, p_key = heapq.heappop(self.heap)
+            del self.dict[p_key]
+        heapq.heappush(self.heap, (self.ctr, key))
 
 
 # Your LRUCache object will be instantiated and called as such:
