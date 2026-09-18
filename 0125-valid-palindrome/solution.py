a1e840c5076c15cf8de42dfe8619cfe1b18b9ecd
Sahil Chugani <59066490+coolstick784@users.noreply.github.com>
2026-09-18 18:05:29 +0000

    [LeetCode Sync] Runtime - 23 ms (5.76%), Memory - 20.7 MB (19.85%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,14 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        letters = set([chr(ord('a') + n) for n in range(26)])
-        nums = set([str(n) for n in range(10)])
-        s = [ch for ch in s if (ch in letters or ch in nums)]
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -=1 
-        return True
+        clean = s.lower()
+        letters = [chr(ch) for ch in range(ord('a'), ord('a') + 26)]
+        numbers = [str(n) for n in range(10)]
+        clean = [ch for ch in clean if ch in letters or ch in numbers]
+        return clean == clean[::-1]
